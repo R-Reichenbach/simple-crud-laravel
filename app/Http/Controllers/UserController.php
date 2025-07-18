d8c260d0e6a108df095e98b49f9813f176e130e6
r-reichenbach <rafareichenbach@gmail.com>
2025-07-17 22:43:57 -0300

    usingFunction2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
  public function register(){
-        return 'Hello from our controller';
+        return 'Hello from other function';
  }
 }
